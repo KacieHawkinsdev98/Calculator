1c9e5a62b1c54f375757f92928a3ef7cf5291df3
Kacie <kaciehawkins1@gmail.com>
2022-06-10 15:26:49 -0600

    added Button, Calculator, OutputScreen, OutputScreenRow, and Title components

---
+++ calculator/src/Components/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Title = (props) => {
+  return (
+
+    <div className="title">
+        {props.value}
+    </div>
+  )
+}
+
+
+export default Title; 
---
+++ calculator/src/index.js
@@ -0,0 +1,6 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './Components/Calculator';
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
